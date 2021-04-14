8375c2ecce882c80b0e3258246d0fd37998ecfd1
SeungJu <sjoh0704@daum.net>
2021-04-14 21:46:37 +0900

    add practice12 - use stack library

---
+++ prac12.cpp
@@ -0,0 +1,25 @@
+//practice12 
+// 앞의 예제에서 “stack.h” 대신 #include <stack> 을 사용해보자 
+#include<iostream>
+#include<stack>
+using namespace std;
+int main() {
+
+	stack<int> s;
+	
+	int tmp;
+	cin >> tmp;
+	while (tmp) {
+		s.push(tmp);
+		cin >> tmp;
+	}
+
+	while (!s.empty()) {
+		
+		
+		cout << s.top()<< endl;
+		s.pop();
+
+	}
+	return 0;
+}

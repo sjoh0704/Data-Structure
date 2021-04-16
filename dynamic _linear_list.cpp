968e8d97043684cde3ae2cf73b77292fc0baa784
SeungJu <sjoh0704@daum.net>
2021-04-16 13:39:32 +0900

    change dynamic linear list format

--- dynamic _linear_list.cpp
+++ dynamic _linear_list.cpp
@@ -1,84 +1,81 @@
-//dynamic linear list
-
+// 다이나믹 리니어 리스트
 #include<iostream>
 #include<assert.h>
 using namespace std;
-typedef int ListElementType;
-class DynamicList {
+typedef char ListElementType;
+class MyList {
 public:
-	DynamicList(int lSize) {
-		assert(lSize>0);
-		listSize = lSize;
-		listArray = new ListElementType[listSize];
-		assert(listArray);
-		numberOfElements = 0;
-		currentPosition = -1;
-		
-	}
-	void insert(const ListElementType& elem) {
-		assert(numberOfElements < listSize);
-		listArray[numberOfElements] = elem;
-		numberOfElements++;
-
-	}
-	bool first(ListElementType& elem) {
-		if (numberOfElements == 0)
-			return false;
-		else {
-			currentPosition = 0;
-			elem = listArray[currentPosition];
-			return true;
-		}
-
-	}
-	bool next(ListElementType& elem) {
-		assert(currentPosition >= 0);
-		if (currentPosition >= numberOfElements -1)
-			return false;
-		else {
-			currentPosition++;
-			elem = listArray[currentPosition];
-			return true;
-		}
-	}
-	int getSize() {
-		return listSize;
-	}
-
+	MyList(int lSize);
+	void insert(const ListElementType& elem);
+	bool first(ListElementType& elem);
+	bool next(ListElementType& elem);
+	int getSize();
 
 private:
-
+	int listSize;
+	ListElementType* listArray;
 	int numberOfElements;
 	int currentPosition;
-	ListElementType* listArray;
-	int listSize;
-
-
-
 
 };
 
-int main() {
+MyList::MyList(int lSize) {
+	assert(lSize >= 0);
+	listSize = lSize;
+	listArray = new ListElementType[listSize];
+	assert(listArray);
+	numberOfElements = 0;
+	currentPosition = -1;
 
-	cout << "input the list size :" << endl;
-	int size;
-	cin >> size;
-	DynamicList l1(size);
-	ListElementType x;
-	cin >> x;
-	while (x) {
-		l1.insert(x);
-		cin >> x;
+}
+void MyList::insert(const ListElementType& elem) {
+	assert(numberOfElements < listSize);
+	listArray[numberOfElements] = elem;
+	numberOfElements++;
+}
+
+bool MyList::first(ListElementType& elem) {
+	if (numberOfElements == 0)
+		return false;
+	else {
+		currentPosition = 0;
+		elem = listArray[currentPosition];
+		return true;
+	}
+}
+
+bool MyList::next(ListElementType& elem) {
+	assert(currentPosition>=0);
+	if (currentPosition >= numberOfElements-1)
+		return false;
+	else {
+		currentPosition++;
+		elem = listArray[currentPosition];
+		return true;
+	}
+}
+int MyList::getSize() {
+	return listSize;
+}
+
+int main() {
+	MyList l(100);
+	char tmp;
+	cin >> tmp;
+	while (tmp != 'q') {
+		l.insert(tmp);
+		cin >> tmp;
 	}
 
-	ListElementType tmp;
-	bool notEmpty = l1.first(tmp);
+	bool notEmpty = l.first(tmp);
 	while (notEmpty) {
 		cout << tmp << endl;
-		notEmpty = l1.next(tmp);
+		notEmpty = l.next(tmp);
+
 	}
 
 
+	
 
 	return 0;
 }

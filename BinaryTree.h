ee11993614c1c22a366adebd520a32023b73458b
SeungJu <sjoh0704@daum.net>
2021-05-13 21:33:09 +0900

    add practice17 - binary tree

---
+++ BinaryTree.h
@@ -0,0 +1,90 @@
+// practice 17
+// binary tree
+#pragma once
+#include<assert.h>
+template<class btElementType>
+class BinaryTree
+{
+public:
+	BinaryTree();
+	bool isEmpty();
+	void insert(const btElementType& data);
+	btElementType getData();
+	BinaryTree* left();
+	BinaryTree* right();
+	void makeLeft(const BinaryTree* T1);
+	void makeRight(const BinaryTree* T2);
+
+
+private:
+	bool nullTree;
+	BinaryTree* leftTree;
+	BinaryTree* rightTree;
+	btElementType data;
+
+};
+
+template<class btElementType>
+BinaryTree<btElementType>::BinaryTree() {
+	nullTree = true;
+	leftTree = 0;
+	rightTree = 0;
+}
+template<class btElementType>
+bool
+BinaryTree<btElementType>::isEmpty() {
+	return nullTree;
+}
+
+template<class btElementType>
+void
+BinaryTree<btElementType>::insert(const btElementType& data) {
+	assert(isEmpty());
+	if (nullTree) {
+		nullTree = false;
+		leftTree = new BinaryTree();
+		rightTree = new BinaryTree();
+		this->data = data;
+	}
+}
+template<class btElementType>
+btElementType
+BinaryTree<btElementType>::getData() {
+	assert(!isEmpty());
+	return data;
+}
+
+template<class btElementType>
+BinaryTree<btElementType>*
+BinaryTree<btElementType>::left(){
+	assert(!isEmpty());
+
+	return leftTree;
+}
+
+template<class btElementType>
+BinaryTree<btElementType>*
+BinaryTree<btElementType>::right() {
+	assert(!isEmpty());
+
+	return rightTree;
+}
+
+
+template<class btElementType>
+void
+BinaryTree<btElementType>::makeLeft(const BinaryTree* T1) {
+	assert(!isEmpty());
+	assert(left().isEmpty());
+	delete left();
+	leftTree = T1;
+}
+
+template<class btElementType>
+void
+BinaryTree<btElementType>::makeRight(const BinaryTree* T2) {
+	assert(!isEmpty());
+	assert(right().isEmpty());
+	delete rightTree;
+	rightTree = T2;
+}
---
+++ prac17.cpp
@@ -0,0 +1,27 @@
+//practice 17
+//binary tree
+#include<iostream>
+#include"BinaryTree.h"
+using namespace std;
+int main() {
+	BinaryTree<char> bt1;
+	bt1.insert('G');
+	bt1.left()->insert('E');
+	bt1.right()->insert('F');
+	bt1.left()->left()->insert('A');
+	bt1.left()->right()->insert('B');
+	bt1.right()->left()->insert('C');
+	bt1.right()->right()->insert('D');
+	
+	cout << bt1.getData() << endl;
+	cout << bt1.left()->getData() << "\t";
+	cout << bt1.right()->getData() << endl;
+	cout << bt1.left()->left()->getData() << "\t";
+	cout << bt1.left()->right()->getData() << "\t";
+	cout << bt1.right()->left()->getData() << "\t";
+	cout << bt1.right()->right()->getData() << endl;
+
+
+
+	return 0;
+}

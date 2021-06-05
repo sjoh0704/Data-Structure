2f755f08d58e62e21bef1d32c072ce81665c91b4
SeungJu <sjoh0704@daum.net>
2021-06-05 21:07:12 +0900

    add practice 20 - topogical sorting with adjacency matrix

---
+++ prac20.cpp
@@ -0,0 +1,38 @@
+//½Ç½À 20 
+#include<iostream>
+#include"uAMGraph.h"
+#include"dAMGraph.h"
+#include<assert.h>
+#include<fstream>
+using namespace std;
+int main() {
+    const char* inFileName = "graph.dat";
+    ifstream ifs(inFileName);
+    assert(ifs); // make sure graph exists
+    int n;
+    ifs >> n;
+
+    uAMGraph G(n);
+    cout << "Created graph; n = " << G.vertexSize() << endl;
+    int u, v;
+    while (ifs >> u) {
+        ifs >> v;
+        G.addEdge(u, v);
+    }
+    cout << "Edges in graph: m = " << G.edgeSize() << endl;
+    int step;
+    for (step = 0; step < n; step++)
+        for (u = 0; u < n; u++)
+            for (v = 0; v < n; v++)
+                if (G.edgeMember(u, step) && G.edgeMember(step, v))
+                    G.addEdge(u, v);
+    // print results
+    for (u = 0; u < n; u++) {
+        cout << u << "\t: ";
+        for (v = 0; v < n; v++)
+            cout << (G.edgeMember(u, v) ? "T " : "F ");
+        cout << endl;
+    }
+    return 0;
+}
+
